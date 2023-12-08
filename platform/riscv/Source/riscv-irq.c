2935eb166b1c57ec642759b5bfeaa9372a40c7d6
Mikhail Liamaev <32442117+kenny5660@users.noreply.github.com>
2023-12-08 18:18:50 +1000

    fix nested irq

--- platform/riscv/Source/riscv-irq.c
+++ platform/riscv/Source/riscv-irq.c
@@ -12,7 +12,7 @@ irqfunc_t* riscv_exception_handler_map[RISCV_EXCP_NUMS];
 
 void riscv_irq_init(void) {
     riscv_irq_global_disable();
-    csr_write_mtvec(trap_handler);
+    csr_write_mtvec((uint32_t*)trap_handler);
     riscv_irq_global_enable();
 }
 
@@ -52,47 +52,52 @@ void riscv_irq_clear_msi(void) {
     *msip0 = 0x0;
 }
 
-static inline void _offset_mepc_to_next_instruction() {
-    uint32_t mepc = csr_read_mepc();
-    if (*((uint8_t*)(mepc)) & 0x3 == 0x3) {
-        csr_write_mepc(mepc + 4);
-    } else {
-        csr_write_mepc(mepc + 2);
-    }
-}
-
 void _default_exception_handler(uint16_t except_num) {
 
     while (1) {};
 }
 
 void trap_handler(void) {
-    uint_xlen_t this_cause = csr_read_mcause();
+    uint_xlen_t mcause = csr_read_mcause();
+    uint_xlen_t mstatus = csr_read_mstatus();
+    uint_xlen_t mepc = csr_read_mepc();
+    uint_xlen_t mie = csr_read_mie();
 
-    if ((this_cause & MCAUSE_INTERRUPT_BIT_MASK) == 0) {
+    
+
+    if ((mcause & MCAUSE_INTERRUPT_BIT_MASK) == 0) {
         // handle exception
-        this_cause &= 0xFF;
-        if (this_cause >= RISCV_EXCP_NUMS) {
+        mcause &= 0xFF;
+        if (mcause >= RISCV_EXCP_NUMS) {
             while (1) {} // error
         }
 
-        if (riscv_exception_handler_map[this_cause] != 0) {
-            riscv_exception_handler_map[this_cause]();
-            _offset_mepc_to_next_instruction();
+        if (riscv_exception_handler_map[mcause] != 0) {
+            riscv_exception_handler_map[mcause]();
         } else {
-            _default_exception_handler(this_cause); // NO handler
+            _default_exception_handler(mcause); // NO handler,
         }
+        // offset mepc to next instruction, considering the length of the instruction.
+        mepc = *((uint8_t*)(mepc)) & 0x3 == 0x3 ? mepc + 4 : mepc + 2; 
     } else {
         // handle interrupt
-        this_cause &= 0xFF;
-        if (this_cause >= RISCV_IRQ_NUMS) {
+        mcause &= 0xFF;
+        if (mcause >= RISCV_IRQ_NUMS) {
             while (1) {} // error
         }
+        riscv_irq_disable(mcause);
+        riscv_irq_global_enable();// enable nested interrupts
 
-        if (riscv_irq_handler_map[this_cause] != 0) {
-            riscv_irq_handler_map[this_cause]();
+        if (riscv_irq_handler_map[mcause] != 0) {
+            riscv_irq_handler_map[mcause]();
         } else {
             while (1) {}; // NO handler
         }
+        riscv_irq_global_disable();
+        riscv_irq_enable(mcause);
     }
+
+    csr_write_mie(mie);
+    csr_write_mstatus(mstatus);
+    csr_write_mepc(mepc);
 }
